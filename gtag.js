9e4d36f786c47f67de41d68788cee63ff49eba5b
trading101tw <121129317+trading101tw@users.noreply.github.com>
2023-06-01 17:04:09 +0800

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DH48L2L9Y9"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DH48L2L9Y9');
+</script>
